51fe1e8e30c34e295b6bf0d5bfd21cacef23c1f8
eight <eight04@gmail.com>
2014-08-17 01:15:40 +0800

    add doxy support

--- grease-info.user.js
+++ grease-info.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Grease Info
-// @version     1.1.1
+// @version     1.1.2
 // @namespace   eight04.blogspot.com
 // @description Parse markdown info in userjs.
 // @include     https://greasyfork.org/scripts/*
@@ -15,6 +15,8 @@
 Experiment implementation!
 
 ## Version History
+*   Version 1.1.2 (Aug 17, 2014)
+	- Add doxygen comment style support.
 *	Version 1.1.1 (Aug 16, 2014)
 	- Update this history.
 *	Version 1.1 (Aug 16, 2014)
@@ -36,15 +38,29 @@ var draw = function(){
 	$("#additional-info>div").html(parsedInfo);
 };
 
-var parse = function(){
-	var RE = /\/\*[* \t]+(markdown)?[* \t]*\r?\n([\u0000-\uffff]+?)\r?\n[* \t]+\*\//m;
-	var m = RE.exec(jsraw);
-	info = m[2];
-	if(m[1]){
-		parsedInfo = marked(info);
-	}else{
-		parsedInfo = $("<pre>" + info + "</pre>");
+var parseDoxy = function(source){
+	var doxyRE = /^ \*(\s|$)/, list, i, usingDoxy = true;
+	
+	list = source.split(/\r?\n/);
+	if(list.length > 1){
+		for(i = 1; i < list.length; i++){
+			if(!doxyRE.test(list[i])){
+				usingDoxy = false;
+				break;
+			}
+		}
 	}
+	if(usingDoxy)){
+		source = source.split(doxyRE).join("");
+	}
+	return source;
+};
+
+var parse = function(){
+	var RE = /\/\*[* \t]+\r?\n([\u0000-\uffff]+?)\r?\n[* \t]*\*\//m;
+	var m = RE.exec(jsraw);
+	info = parseDoxy(m[1]);
+	parsedInfo = marked(info);
 	
 	draw();
 };
