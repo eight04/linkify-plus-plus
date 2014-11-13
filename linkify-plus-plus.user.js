87abb182a0d4149a70252d45a9618078640c8200
eight <eight04@gmail.com>
2014-11-14 02:53:30 +0800

    Fix comment element bug

--- linkify-plus-plus.user.js
+++ linkify-plus-plus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Linkify Plus Plus
-// @version     3.0.2
+// @version     3.0.3
 // @namespace   eight04.blogspot.com
 // @description Based on Linkify Plus. Turn plain text URLs into links.
 // @include     http*
