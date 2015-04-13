dff12e8fe018b7f9f745d3a679285aa49f550bbd
eight <eight04@gmail.com>
2015-04-13 17:30:34 +0800

    3.4.0 build

--- linkify-plus-plus.user.js
+++ linkify-plus-plus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Linkify Plus Plus
-// @version     3.3.0
+// @version     3.4.0
 // @namespace   eight04.blogspot.com
 // @description Based on Linkify Plus. Turn plain text URLs into links.
 // @include     http*
