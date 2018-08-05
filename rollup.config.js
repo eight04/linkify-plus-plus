dae67b250e80e40a10a53416207b7bebe20ef6c1
eight <eight04@gmail.com>
2018-08-05 09:04:02 +0800

    Add: build script

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import glob from "glob";
+import resolve from "rollup-plugin-node-resolve";
+import cjs from "rollup-plugin-cjs-es";
+
+function base({
+  output,
+  ...props
+}) {
+  return {
+    output: {
+      format: "es",
+      ...output
+    },
+    plugins: [
+      resolve(),
+      cjs()
+    ],
+    experimentalCodeSplitting: true,
+    ...props
+  };
+}
+
+export default [
+  base({
+    input: glob.sync("src/*.js"),
+    output: {
+      dir: "extension/js",
+      format: "es"
+    }
+  }),
+  base({
+    input: "src/userscript.js",
+    output: {
+      file: "dist/linkify-plus-plus.user.js",
+    }
+  })
+];
